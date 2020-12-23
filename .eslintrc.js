c6de3c55ffa0ed57baf364409c201b28ce4c1401
王闯 <wangchuang@ymt360.com>
2020-12-23 11:33:51 +0800

    添加eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,17 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
     sourceType: 'module',
     allowImportExportEverywhere: false,
     codeFrame: true,
+    // ecmaFeature: { //想使用额外的语言特性
+    //   "jsx": true, //启用jsx
+    //   "globalReturn": true, //在全局作用域下使用return语句
+    //   "impliedStrict": true, //启用全局strict mode
+    //   "experimentalObjectRestSpread": false
+    //   //启用实验性的object rest/spread properties支持
+    //   //(不建议开启)
+    // }
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
@@ -22,5 +29,21 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-types': [
+      'error',
+      {
+        types: {
+          String: false,
+          Boolean: false,
+          Number: false,
+          Symbol: false,
+          '{}': false,
+          Object: false,
+          object: false,
+          Function: false,
+        },
+        extendDefaults: true,
+      },
+    ],
   },
 };
