f241582d8ca5c20034fde219aa2e74f7e5bed58a
pt <pt_liangyue@outlook.com>
2021-01-03 13:50:21 +0800

    update users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,18 @@
 import { Controller, Get, HttpStatus, Response } from '@nestjs/common';
+import { ApiHeader, ApiTags } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
+@ApiTags('users')
+@ApiHeader({
+  name: 'Authorization',
+  description: '用户 token',
+})
 @Controller('users')
 export class UsersController {
+  constructor(private configService: ConfigService) {
+    console.log(this.configService.get('base.port'));
+    console.log(this.configService.get('mysql.password'));
+  }
   @Get()
   getUser(@Response() res) {
     res.status(HttpStatus.OK).json({});
