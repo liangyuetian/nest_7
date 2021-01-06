e417fe2ea1b6c033aaa778ece6e40c2eb04e8a5b
王闯 <wangchuang@ymt360.com>
2021-01-06 19:57:55 +0800

    添加 StressTestApiService

--- src/stress-test-api/stress-test-api.controller.ts
+++ src/stress-test-api/stress-test-api.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Response } from '@nestjs/common';
+import { ApiProperty } from '@nestjs/swagger';
+import { StressTestApiService } from './stress-test-api.service';
 
 @Controller('stress-test-api')
-export class StressTestApiController {}
+export class StressTestApiController {
+  constructor(private stressTestApiService: StressTestApiService) {}
+
+  @Get('set-api')
+  getSetApiProperty(@Response() res) {
+    this.stressTestApiService.getSetApiPropertyTime().subscribe((data) => {
+      res.status(HttpStatus.OK).send({ msg: `使用装饰api耗时：${data}` });
+    });
+  }
+  @Get('no-api')
+  getNoApiProperty(@Response() res) {
+    this.stressTestApiService.getNoApiPropertyTime().subscribe((data) => {
+      res.status(HttpStatus.OK).send({ msg: `不使用装饰api耗时：${data}` });
+    });
+  }
+}
--- src/stress-test-api/stress-test-api.module.ts
+++ src/stress-test-api/stress-test-api.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { StressTestApiController } from './stress-test-api.controller';
+import { StressTestApiService } from './stress-test-api.service';
 
 @Module({
-  controllers: [StressTestApiController]
+  controllers: [StressTestApiController],
+  providers: [StressTestApiService]
 })
 export class StressTestApiModule {}
