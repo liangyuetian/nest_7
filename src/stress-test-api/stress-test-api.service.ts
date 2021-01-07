10b8869f937c898eef1b1a14a919bc61e20dd712
王闯 <wangchuang@ymt360.com>
2021-01-07 18:07:28 +0800

    使用rxjs统计发送 10000 条请求的耗时

--- src/stress-test-api/stress-test-api.service.ts
+++ src/stress-test-api/stress-test-api.service.ts
@@ -1,43 +1,33 @@
 import { HttpService, Injectable } from '@nestjs/common';
-import { of, forkJoin } from 'rxjs';
+import { of } from 'rxjs';
 import { ConfigService } from '@nestjs/config';
-import { concatMap, delay, delayWhen, map } from 'rxjs/operators';
+import { delayWhen, expand, last, map, take } from 'rxjs/operators';
 
 @Injectable()
 export class StressTestApiService {
   port = 0;
+  take = 10000;
 
   constructor(
     private httpService: HttpService,
     private configService: ConfigService,
   ) {
     this.port = this.configService.get('base.port');
-    // const source = of(2000, 1000);
-    // // 将内部 observable 映射成 source，当前一个完成时发出结果并订阅下一个
-    // const example = source.pipe(
-    //   concatMap((val) => of(`Delayed by: ${val}ms`).pipe(delay(val))),
-    // );
-    // // 输出: With concatMap: Delayed by: 2000ms, With concatMap: Delayed by: 1000ms
-    // const subscribe = example.subscribe((val) =>
-    //   console.log(`With concatMap: ${val}`),
-    // );
   }
 
+  // 使用装饰api
   getApiPropertyTimeSource$() {
-    return forkJoin(
-      Array.from(Array(10), () =>
-        this.httpService.post(
-          `http://127.0.0.1:${this.port}/swagger/tutorials`,
-          {
-            id: 2,
-            name: '哈哈',
-            email: 'pt_liangyue@outlook.com',
-          },
-        ),
-      ),
+    return this.httpService.post(
+      `http://127.0.0.1:${this.port}/swagger/tutorials`,
+      {
+        id: 2,
+        name: '哈哈',
+        email: 'pt_liangyue@outlook.com',
+      },
     );
   }
 
+  // 不使用装饰api
   getNoApiPropertyTimeSource$() {
     return this.httpService.post(
       `http://127.0.0.1:${this.port}/swagger/no-property`,
@@ -49,21 +39,27 @@ export class StressTestApiService {
     );
   }
 
+  // 使用装饰api
   getSetApiPropertyTime() {
     return of(Date.now()).pipe(
-      delayWhen(() => this.getApiPropertyTimeSource$()),
-      map((data) => {
-        return Date.now() - data;
-      }),
+      expand((time) =>
+        of(time).pipe(delayWhen(() => this.getApiPropertyTimeSource$())),
+      ),
+      take(this.take),
+      last(),
+      map((time) => [Date.now() - time, this.take]),
     );
   }
 
+  // 不使用装饰api
   getNoApiPropertyTime() {
     return of(Date.now()).pipe(
-      delayWhen(() => this.getNoApiPropertyTimeSource$()),
-      map((data) => {
-        return Date.now() - data;
-      }),
+      expand((time) =>
+        of(time).pipe(delayWhen(() => this.getNoApiPropertyTimeSource$())),
+      ),
+      take(this.take),
+      last(),
+      map((time) => [Date.now() - time, this.take]),
     );
   }
 }
