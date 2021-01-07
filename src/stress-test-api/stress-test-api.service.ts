48ffa25584f570756e540a0d8c8201c4c9eb4fec
王闯 <wangchuang@ymt360.com>
2021-01-07 11:07:08 +0800

    添加单个请求时间

--- src/stress-test-api/stress-test-api.service.ts
+++ src/stress-test-api/stress-test-api.service.ts
@@ -1,11 +1,12 @@
 import { HttpService, Injectable } from '@nestjs/common';
-import { Observable, of } from 'rxjs';
+import { of } from 'rxjs';
 import { ConfigService } from '@nestjs/config';
-import { map } from 'rxjs/operators';
+import { delayWhen, map } from 'rxjs/operators';
 
 @Injectable()
 export class StressTestApiService {
   port = 0;
+
   constructor(
     private httpService: HttpService,
     private configService: ConfigService,
@@ -13,31 +14,43 @@ export class StressTestApiService {
     this.port = this.configService.get('base.port');
   }
 
-  getSetApiPropertyTime() {
-    return new Observable((subscriber) => {
-      const time = Date.now();
-      this.httpService
-        .post(`http://127.0.0.1:${this.port}/swagger/tutorials`, {
-          id: 2,
-          name: '哈哈',
-          email: 'pt_liangyue@outlook.com',
-        })
-        .subscribe(() => {
-          subscriber.next(Date.now() - time);
-        });
-    });
-  }
-
-  getNoApiPropertyTime() {
-    this.httpService
-      .post(`http://127.0.0.1:${this.port}/swagger/no-property`, {
+  getApiPropertyTimeSource$() {
+    return this.httpService.post(
+      `http://127.0.0.1:${this.port}/swagger/tutorials`,
+      {
         id: 2,
         name: '哈哈',
         email: 'pt_liangyue@outlook.com',
-      })
-      .subscribe((res) => {
-        // console.log(11);
-      });
-    return of(20);
+      },
+    );
+  }
+
+  getNoApiPropertyTimeSource$() {
+    return this.httpService.post(
+      `http://127.0.0.1:${this.port}/swagger/no-property`,
+      {
+        id: 2,
+        name: '哈哈',
+        email: 'pt_liangyue@outlook.com',
+      },
+    );
+  }
+
+  getSetApiPropertyTime() {
+    return of(Date.now()).pipe(
+      delayWhen(() => this.getApiPropertyTimeSource$()),
+      map((data) => {
+        return Date.now() - data;
+      }),
+    );
+  }
+
+  getNoApiPropertyTime() {
+    return of(Date.now()).pipe(
+      delayWhen(() => this.getNoApiPropertyTimeSource$()),
+      map((data) => {
+        return Date.now() - data;
+      }),
+    );
   }
 }
