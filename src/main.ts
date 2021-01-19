49934740b5d071460bb534034c302d0f758da8f1
王闯 <wangchuang@ymt360.com>
2021-01-19 15:18:22 +0800

    express 替换为 fastify

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,23 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { ConfigService } from '@nestjs/config';
+import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './core/filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: ['error', 'warn'],
-    // 是否在错误时中止该过程。默认情况下，该过程被退出。传递false可以覆盖默认行为。如果传递了false，则Nest不会退出应用程序，而是会重新引发异常。
-    abortOnError: false,
-  });
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+    {
+      logger: ['error', 'warn'],
+      // 是否在错误时中止该过程。默认情况下，该过程被退出。传递false可以覆盖默认行为。如果传递了false，则Nest不会退出应用程序，而是会重新引发异常。
+      abortOnError: false,
+    },
+  );
 
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
