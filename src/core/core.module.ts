dde4ecf4b744433cddf17313dbaaa19ba5fca436
王闯 <wangchuang@ymt360.com>
2021-02-20 10:36:04 +0800

    添加 email.service.ts

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -2,6 +2,7 @@ import { Module, HttpAdapterHost, Global } from '@nestjs/common';
 import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { HttpSuccessInterceptor } from './interceptor';
 import { AllExceptionsFilter, HttpExceptionFilter } from './filter';
+import { EmailService } from './email/email.service';
 
 @Module({
   providers: [
@@ -13,6 +14,8 @@ import { AllExceptionsFilter, HttpExceptionFilter } from './filter';
       provide: APP_FILTER,
       useClass: HttpExceptionFilter,
     },
+    EmailService,
   ],
+  exports: [EmailService],
 })
 export class CoreModule {}
---
+++ src/core/email/email.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import { createTestAccount, createTransport, TestAccount } from 'nodemailer';
+
+// 发送邮件
+@Injectable()
+export class EmailService {
+  testAccount: TestAccount;
+  static createTransport = createTransport;
+  constructor() {
+    this.sendMail({});
+  }
+
+  async getTestAccount() {
+    this.testAccount = await createTestAccount();
+    console.log('创建测试邮箱服务器成功', this.testAccount);
+    return this.testAccount;
+  }
+
+  async getTransporter() {
+    await this.getTestAccount();
+    return createTransport({
+      host: this.testAccount.smtp.host,
+      port: this.testAccount.smtp.port,
+      auth: {
+        user: this.testAccount.user,
+        pass: this.testAccount.pass,
+      },
+    });
+  }
+
+  async sendMail({}) {
+    const transporter = await this.getTransporter();
+    return transporter
+      .sendMail({
+        from: `"${this.testAccount.user}" ${this.testAccount.user}`,
+        to: 'wangchuang@ymt360.com',
+        subject: 'nodemailer',
+        html: '<h1>测试 nodemailer</h1>',
+      })
+      .then((res) => {
+        console.log('res', res);
+      })
+      .catch((err) => {
+        console.log('err', err);
+      });
+  }
+}
