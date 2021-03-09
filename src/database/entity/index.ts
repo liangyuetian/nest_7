f045a0b2d88860cae08d2e9f3572e588b0982de7
王闯 <wangchuang@ymt360.com>
2021-03-09 18:01:26 +0800

    del

--- src/database/entity/index.ts
+++ src/database/entity/index.ts
@@ -1,5 +1,1 @@
-export * from './activity.entity';
-export * from './participant.entity';
-export * from './share-record.entity';
-export * from './profit-record.entity';
-export * from './consumption-record.entity';
+//
